32286a86c8e03b1715bb681859a3344c82465cc1
Bhanuka Shamen Rathnasiri <120768729+shamenrathnasiri@users.noreply.github.com>
2024-08-13 15:46:57 +0530

    change navbar components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -29,18 +29,18 @@ const Navbar = () => {
             </svg>
           </button>
           <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 font-medium border border-green-100 rounded-lg md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 bg-[#A7FFBB] md:bg-[#A7FFBB] dark:bg-[#A7FFBB] md:dark:bg-[#A7FFBB] dark:border-[#A7FFBB]">
+            <ul className="flex flex-col p-4 mt-4 font-medium md:p-0 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
               <li>
-                <a href="/Home" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</a>
+                <a href="/Home" className="block px-3 py-2 text-black rounded md:hover:text-green-700 md:p-0 ">Home</a>
               </li>
               <li>
-                <a href="/Menu" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Menu</a>
+                <a href="/Menu" className="block px-3 py-2 text-black rounded md:hover:text-green-700 md:p-0 ">Menu</a>
               </li>
               <li>
-                <a href="/About" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About us</a>
+                <a href="/About" className="block px-3 py-2 text-black rounded md:hover:text-green-700 md:p-0 ">About us</a>
               </li>
               <li>
-                <a href="/contact" className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 md:p-0 dark:text-black md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+                <a href="/contact" className="block px-3 py-2 text-black rounded md:hover:text-green-700 md:p-0 ">Contact</a>
               </li>
             </ul>
           </div>
